5bf49084b4029c9370c47aaeb5de39f9e5f2e39a
Pponghub <wallop.wwww@gmail.com>
2023-08-12 14:42:02 +0700

    0s ToT

--- timing.c
+++ timing.c
@@ -2,14 +2,22 @@
 #include<time.h>
 
 int main(){
-    int n;
-    time_t begin = time(NULL);
+    int n=0,m=0;
+    time_t begin1 = time(NULL);
     for(int i=0;i<1000000;i++){
-        n=n+1;
-        printf("%d",&n);
+        n+=1;
+        printf("%d",n);
     }
-    time_t end = time(NULL);
+    time_t end1 = time(NULL);
     printf("\n");
-    printf("The elapsed time is %d seconds", (end - begin));
+    printf("The elapsed time is %d seconds", (end1 - begin1));
+
+    time_t begin2 = time(NULL);
+    for(int i=0;i<1000000;i++){
+        m+=1;
+    }
+    time_t end2 = time(NULL);
+    printf("\n");
+    printf("The elapsed time is %d seconds", (end2 - begin2));
     return 0;
 }
